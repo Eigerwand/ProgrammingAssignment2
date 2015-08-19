a198e7226f13f529109db50e0d8dabfac98da378
Buel Chandler <kosmische@yahoo.com>
2015-08-19 13:58:27 -0400

    2nd draft. Seems to work on first test case

--- cachematrix.R
+++ cachematrix.R
@@ -16,12 +16,22 @@ makeCacheMatrix <- function(x = matrix()) {
     getinverse <- function() inverse # returns the inverse
     
     # return the functions for use in calling environment
-    list(set = set, get = get, setinverse =setinverse, getinverse = getinverse)
+    list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse() # whats in current instantiation
+    
+    if(!is.null(m)) { # It has already been computed
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get() # else we need to compute it
+    m <- solve(data, ...) # assume that data invertable
+    x$setinverse(m) # update the enviroment
+    m            # return the inverse
 }
