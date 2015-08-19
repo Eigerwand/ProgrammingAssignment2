a8ff66d4452ac02837b5432b7c41fb119b176950
Buel Chandler <kosmische@yahoo.com>
2015-08-19 13:03:10 -0400

    1st draft

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## here wedefinethe funstions that manipulate the needed variables and 
+## functions within the environment from where we execute the returned functions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL # The initial inverse doesn't exist
+    
+    set <- function(y) { # set the value of the matrix and it's inverse
+        x <<- y
+        inverse <<- NULL
+    }
+    
+    get <- function() x # returns the defined matrix
+    
+    setinverse <- function(created) inverse <<- created # sets the inverse
+    
+    getinverse <- function() inverse # returns the inverse
+    
+    # return the functions for use in calling environment
+    list(set = set, get = get, setinverse =setinverse, getinverse = getinverse)
 }
 
 
